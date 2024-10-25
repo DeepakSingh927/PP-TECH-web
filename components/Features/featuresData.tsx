58d994ff410b75808f6c646505969b04e0645554
Parvesh93 <parveshdesigns21@gmail.com>
2024-10-26 02:09:49 +0530

    Initial commit

---
+++ components/Features/featuresData.tsx
@@ -0,0 +1,30 @@
+import { Feature } from "@/types/feature";
+
+const featuresData: Feature[] = [
+  {
+    id: 1,
+    icon: "/images/icon/icon-01.svg",
+    title: "Shopify Development",
+    description:
+      "Tailored Shopify solutions to create scalable, high-performing online stores.",
+  },
+  {
+    id: 2,
+    icon: "/images/icon/icon-02.svg",
+    title: "Wordpress Development",
+    description:
+      "Custom WordPress websites designed to engage and grow your audience.",
+  },
+  {
+    id: 3,
+    icon: "/images/icon/icon-03.svg",
+    title: "Website Maintenance",
+    description:
+      "Ensure your website stays updated, secure, and optimized with our maintenance services.",
+  },
+
+
+  
+];
+
+export default featuresData;
