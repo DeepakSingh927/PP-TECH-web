54803a1ba39b9362461c15e3d868e101e7ea1647
Parvesh93 <parveshdesigns21@gmail.com>
2024-11-10 21:33:32 +0530

    update

--- components/WebPortfolio/portfolioData.ts
+++ components/WebPortfolio/portfolioData.ts
@@ -1,55 +1,101 @@
-import about1 from "@/public/images/about/about1.png";
-import image2 from "@/public/images/user/user-02.png";
+import pyarefoundation from "@/public/images/portfolio/pyarefoundation.png";
+import kcons from "@/public/images/portfolio/kcons.png";
+import shivaaysigns from "@/public/images/portfolio/shivaaysigns.png";
+import krishnasangsang from "@/public/images/portfolio/krishnasangsang.png";
+import pratham from "@/public/images/portfolio/pratham.png";
+import ics from "@/public/images/portfolio/ics.png";
+import ascendus from "@/public/images/portfolio/ascendus.png";
+import vistahomeware from "@/public/images/portfolio/vistahomeware.png";
+import gsnaturale from "@/public/images/portfolio/gsnaturale.png";
+import volcape from "@/public/images/portfolio/volcape.png";
+import nattier from "@/public/images/portfolio/nattier.png";
+import bbf from "@/public/images/portfolio/bbf.png";
+import tasveer from "@/public/images/portfolio/tasveer.png";
+import nanis from "@/public/images/portfolio/nanis.png";
+
 import { Portfolio } from "@/types/portfolio";
 
 
 export const PortfolioData: Portfolio[] = [
   {
     id: 1,
-    image: about1,
-    name: "Project 1",
+    image: pyarefoundation,
+    name: "Pyare Foundation",
   },
 
   {
     id: 2,
-    image: about1,
-    name: "Project 2",
+    image: kcons,
+    name: "Kurkure Consulting",
   },
 
   {
     id: 3,
-    image: about1,
-    name: "Project 3",
+    image: shivaaysigns,
+    name: "Shivaay Signs",
   },
   {
     id: 4,
-    image: about1,
-    name: "Project 3",
+    image: krishnasangsang,
+    name: "Krishnasangsang",
   },
 
   {
     id: 5,
-    image: about1,
-    name: "Project 1",
+    image: pratham,
+    name: "Pratham Delhi",
   },
 
   {
     id: 6,
-    image: about1,
-    name: "Project 2",
+    image: ics,
+    name: "ICS Intelligent",
   },
 
   {
     id: 7,
-    image: about1,
-    name: "Project 3",
+    image: ascendus,
+    name: "ascendus",
   },
   {
     id: 8,
-    image: about1,
-    name: "Project 3",
+    image: vistahomeware,
+    name: "Vista Homeware",
   },
 
+  {
+    id: 9,
+    image: gsnaturale,
+    name: "Gs Naturale",
+  },
 
+  {
+    id: 10,
+    image: volcape,
+    name: "Volcape",
+  },
+
+  {
+    id: 11,
+    image: nattier,
+    name: "nattier!",
+  },
+
+  {
+    id: 12,
+    image: bbf,
+    name: "Big Bear Farms",
+  },
+  {
+    id: 13,
+    image: tasveer,
+    name: "Tasveer",
+  },
+
+  {
+    id: 14,
+    image: nanis,
+    name: "Nanis Potion",
+  },
 
   ];
